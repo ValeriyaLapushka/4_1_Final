f846bc152a0e4fcb1467e0e6e1cb86b8963ba9a2
ValeriyaLapushka <boldinavaleriya@mail.ru>
2019-07-27 16:23:59 +0300

    Сделали отрицательные проверки:)

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
 from .locators import BasePageLocators
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage(object):
 	def __init__(self, browser, url, timeout=10):
@@ -18,7 +21,14 @@ class BasePage(object):
 		except (NoSuchElementException):
 			return False
 		return True	
-		
+	
+	def is_not_element_present(self, how, what, timeout=4):
+		try:
+			WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+		except TimeoutException:
+			return True
+
+		return False	
 		
 	def go_to_login_page(self):
 		try:
@@ -28,5 +38,26 @@ class BasePage(object):
 		return True	
 	
 
-	def should_be_login_link(self):
-		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылки нет"
+		
+	def is_disappeared(self, how, what, timeout=4):
+		try:
+			WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how , what)))
+		except TimeoutException:
+			return False
+
+		return True	
+		
+		
+		
+	def solve_quiz_and_get_code(self):
+		alert = self.browser.switch_to.alert
+		x = alert.text.split(" ")[2]
+		answer = str(math.log(abs((12 * math.sin(float(x))))))
+		alert.send_keys(answer)
+		alert.accept()
+		try:
+			alert = self.browser.switch_to.alert
+			print("Your code: {}".format(alert.text))
+			alert.accept()
+		except NoAlertPresentException:
+			print("No second alert presented")	
--- pages/product_page.py
+++ pages/product_page.py
@@ -3,9 +3,7 @@ from pages.base_page import BasePage
 from .locators import MainPageLocators, AddProduct, NotBeSuccess
 from selenium.common.exceptions import NoAlertPresentException
 import math
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class PageObject(BasePage):
@@ -26,45 +24,21 @@ class PageObject(BasePage):
 		
 		
 		
-	def solve_quiz_and_get_code(self):
-		alert = self.browser.switch_to.alert
-		x = alert.text.split(" ")[2]
-		answer = str(math.log(abs((12 * math.sin(float(x))))))
-		alert.send_keys(answer)
-		alert.accept()
-		try:
-			alert = self.browser.switch_to.alert
-			print("Your code: {}".format(alert.text))
-			alert.accept()
-		except NoAlertPresentException:
-			print("No second alert presented")
+	def should_be_login_link(self):
+		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Ссылки нет"
 			
 	
 
-	#def should_not_be_success_message(self):
-	#	
-	##	assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+	def should_not_be_success_message(self):
+		assert self.is_not_element_present(*NotBeSuccess.SUCCESS_ADD_PRODUCT), "Success message is presented, but should not be"
 		
 		
 		
+	def should_not_be_success_message_for_is_disappeared(self):
+		assert self.is_disappeared(*NotBeSuccess.SUCCESS_ADD_PRODUCT), "Success message is presented, but should not be"
 		
 		
-	# def is_disappeared(self, how, what, timeout=4):
-		# try:
-			# WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located(how, what))
-		# except TimeoutException:
-			# return False
 
-		# return True	
-		
-		
-	# def is_not_element_present(self,how, what, timeout=4):
-		# try:
-			# WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located(By.PARTIAL_LINK_TEXT, 'has been added to your basket'))
-		# except TimeoutException:
-			# return True
-
-		# return False	
 		
 		
 	
